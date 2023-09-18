9240d5002aeeed0a6073011796f01d007ec59440
Kaiky Pires <129606756+KaikyPires@users.noreply.github.com>
2023-09-18 20:17:58 -0300

    Add files via upload

---
+++ AEDS.java
@@ -0,0 +1,26 @@
+
+package aeds;
+
+import java.util.Scanner;
+
+public class AEDS {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        
+        int x = teclado.nextInt();
+         int aux = x;
+         
+            if(x == 1){
+                x = 1;
+            }
+            while (aux > 1){
+               
+                 x = x * ( aux - 1);
+                 aux = aux - 1;   
+                    
+            }
+            System.out.println(x+"");
+    }
+    
+}
